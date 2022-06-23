c54bfca9f1b411f6436cfb0e71766ea4e854382c
polmatambunan <polma.tambunan@fit.id>
2022-06-23 19:47:38 +0700

    initial project

--- src/App.js
+++ src/App.js
@@ -1,24 +1,31 @@
-import logo from './logo.svg';
-import './App.css';
+import { React } from "react";
+import { Form, Button, Container } from "react-bootstrap";
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <Container style={{ display: "block", width: 700, padding: 30 }}>
+      <h4 className="text-center">React-Bootstrap Form Component</h4>
+      <Form>
+        <Form.Group>
+          <Form.Label>full name:</Form.Label>
+          <Form.Control type="text" placeholder="full name" />
+        </Form.Group>
+        <Form.Group>
+          <Form.Label>email address:</Form.Label>
+          <Form.Control type="email" placeholder="your email address" />
+        </Form.Group>
+        <Form.Group>
+          <Form.Label>age:</Form.Label>
+          <Form.Control type="number" placeholder="age" />
+        </Form.Group>
+        <br />
+        <div className="text-center">
+          <Button variant="primary" type="submit">
+            Click here to submit form
+          </Button>
+        </div>
+      </Form>
+    </Container>
   );
 }
 
